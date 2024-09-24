9401265767b7aab589f52be88568c5ffa9a27888
Florian LE GALOUDEC <pti.breton@hotmail.fr>
2024-09-25 00:58:53 +0200

    feat: modification section resume + ajout media query

--- main.js
+++ main.js
@@ -8,3 +8,12 @@ links.forEach(link => {
         this.classList.add('active');
     });
 });
+
+// --------------  calcul taille header ------------------- //
+const header = document.querySelector('.header');
+const sections = document.querySelectorAll('section');
+const headerHeight = header.offsetHeight;
+
+sections.forEach(section => {
+    section.style.marginTop = headerHeight + 'px';
+});
